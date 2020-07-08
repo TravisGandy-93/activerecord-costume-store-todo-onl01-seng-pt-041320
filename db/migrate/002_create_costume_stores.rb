494d0633a3a360072f9d2eef9a8b1e4aee54b8f6
Travis Gandy <travis_gandy@yahoo.com>
2020-07-08 16:30:59 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,10 +4,11 @@ class CreateCostumeStores < ActiveRecord::Migration[5.2]
     create_table :costume_stores do |t|
       t.string :name
       t.string :location
-      t.integer :price
-      t.string :size 
-      t.string :image_url
-      t.timestamps
+      t.integer :costume_inventory
+      t.integer :num_of_employees
+      t.boolean :still_in_business
+      t.datetime :opening_time
+      t.datetime :closing_time
     end
   end
 end
